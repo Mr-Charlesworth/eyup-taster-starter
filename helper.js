0d39ef05063e9ee01ba24c0912be89799ceba9d5
Andrew Charlesworth <andrewcharlesworth@andysmac.local>
2023-02-22 09:11:45 +0000

    initial commit

---
+++ helper.js
@@ -0,0 +1,7 @@
+const hitCounter = document.getElementById('hit-counter');
+const walls = document.querySelectorAll('.wall');
+const resetBtn = document.getElementById('btn-reset');
+const finishBtn = document.getElementById('btn-finish');
+const timeElapsedEl = document.getElementById('time-elapsed');
+const startBtn = document.getElementById('btn-start');
+const body = document.getElementById('body');
